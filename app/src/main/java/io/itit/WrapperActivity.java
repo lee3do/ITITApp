91cbdb1952992548d8db2cbb7ce3da51344b0805
lee3do@gmail.com <Ll0321670>
2016-12-11 18:36:35 +0800

    加入视差状态栏

--- app/src/main/java/io/itit/WrapperActivity.java
+++ app/src/main/java/io/itit/WrapperActivity.java
@@ -3,17 +3,26 @@ package io.itit;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.orhanobut.logger.Logger;
 import com.umeng.socialize.ShareAction;
 import com.umeng.socialize.UMShareAPI;
@@ -24,11 +33,7 @@ import com.umeng.socialize.shareboard.ShareBoardConfig;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import in.srain.cube.views.ptr.PtrClassicFrameLayout;
-import in.srain.cube.views.ptr.PtrDefaultHandler;
-import in.srain.cube.views.ptr.PtrFrameLayout;
-import in.srain.cube.views.ptr.PtrHandler;
-import in.srain.cube.views.ptr.header.StoreHouseHeader;
+import cn.trinea.android.common.util.StringUtils;
 import io.itit.http.HttpUtils;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 import rx.android.schedulers.AndroidSchedulers;
@@ -37,12 +42,20 @@ import rx.schedulers.Schedulers;
 import static io.itit.http.HttpUtils.iconUrl;
 
 public class WrapperActivity extends SwipeBackActivity {
+
+    private ImageLoader imageLoader = ImageLoader.getInstance();
+    private DisplayImageOptions options = new DisplayImageOptions.Builder().showImageOnLoading(R
+            .drawable.noimage).showImageOnFail(R.drawable.noimage).showImageForEmptyUri(R
+            .drawable.lks_for_blank_url).cacheInMemory(true).cacheOnDisk(true).considerExifParams
+            (true).build();
+
     String url = "";
     String urlFirst = "";
     String title = "";
     int id;
     boolean isLiked = false;
     boolean isReadOri = false;
+    boolean showHeadFirst = false;
     MenuItem favMenu;
     MenuItem readOriMenu;
 
@@ -50,15 +63,25 @@ public class WrapperActivity extends SwipeBackActivity {
     Toolbar toolbar;
     @Bind(R.id.webView)
     WebView webView;
-    @Bind(R.id.rotate_header_web_view_frame)
-    PtrClassicFrameLayout rotateHeaderWebViewFrame;
+    @Bind(R.id.collapsing_toolbar_layout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @Bind(R.id.head_image)
+    ImageView headImage;
+    @Bind(R.id.appBar)
+    AppBarLayout appBar;
+    @Bind(R.id.scroll_view)
+    NestedScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wrapper);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //透明导航栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
         if (getIntent() != null) {
             url = getIntent().getStringExtra("URL");
             urlFirst = url;
@@ -66,15 +89,16 @@ public class WrapperActivity extends SwipeBackActivity {
             id = getIntent().getIntExtra("ID", 0);
         }
         initWebview();
-        initPullToRefresh();
+        // initPullToRefresh();
         webView.loadUrl(url);
-        toolbar.setTitle(title);
+
+        initActionBar();
 
         HttpUtils.appApis.isFav(ITITApplication.uuid, id).subscribeOn(Schedulers.io()).observeOn
                 (AndroidSchedulers.mainThread()).subscribe(info -> {
             isLiked = info.isIsLike();
-            runOnUiThread(()->{
-                Logger.d("this page is liked?"+isLiked);
+            runOnUiThread(() -> {
+                Logger.d("this page is liked?" + isLiked);
                 if (isLiked) {
                     favMenu.setIcon(R.drawable.ic_like);
                 } else {
@@ -84,6 +108,31 @@ public class WrapperActivity extends SwipeBackActivity {
         }, error -> {
             Logger.e(error.getLocalizedMessage());
         });
+
+    }
+
+    private void initActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+        mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);//设置收缩后Toolbar上字体的颜色
+        mCollapsingToolbarLayout.setTitle("  ");
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.md_nav_back);
+        upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        toolbar.setTitle("  ");
+        if (!StringUtils.isEmpty(ITITApplication.displayedItem.getImgUrl())) {
+            showHeadFirst = true;
+            imageLoader.displayImage(ITITApplication.displayedItem.getImgUrl(), headImage, options);
+//        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.actionbarTextStyle);
+//        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.actionbarTextStyle);
+        } else {
+            appBar.setExpanded(false, false);
+            scrollView.setNestedScrollingEnabled(false);
+        }
+
 
     }
 
@@ -117,9 +166,16 @@ public class WrapperActivity extends SwipeBackActivity {
             case R.id.read_orign:
                 isReadOri = !isReadOri;
                 if (isReadOri) {
+                    appBar.setExpanded(false, true);
+                    scrollView.setNestedScrollingEnabled(false);
                     webView.loadUrl(ITITApplication.displayedItem.getUrl());
                     readOriMenu.setIcon(R.drawable.read_empty);
                 } else {
+                    if (showHeadFirst) {
+                        appBar.setExpanded(true, true);
+                        scrollView.setNestedScrollingEnabled(false);
+                    }
+
                     webView.loadUrl(urlFirst);
                     readOriMenu.setIcon(R.drawable.read_full);
                 }
@@ -166,30 +222,30 @@ public class WrapperActivity extends SwipeBackActivity {
         }).open(config);
     }
 
-    private void initPullToRefresh() {
-        StoreHouseHeader header = new StoreHouseHeader(this);
-        header.initWithString("ITIT");
-        rotateHeaderWebViewFrame.setHeaderView(header);
-        rotateHeaderWebViewFrame.addPtrUIHandler(header);
-
-        rotateHeaderWebViewFrame.disableWhenHorizontalMove(true);
-
-        rotateHeaderWebViewFrame.setPtrHandler(new PtrHandler() {
-            @Override
-            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
-                boolean canRefresh = PtrDefaultHandler.checkContentCanBePulledDown(frame,
-                        content, header);
-                return canRefresh;
-            }
-
-            @Override
-            public void onRefreshBegin(PtrFrameLayout frame) {
-                webView.loadUrl(url);
-                rotateHeaderWebViewFrame.postDelayed(() -> rotateHeaderWebViewFrame
-                        .refreshComplete(), 10000);//默认10s关闭
-            }
-        });
-    }
+//    private void initPullToRefresh() {
+//        StoreHouseHeader header = new StoreHouseHeader(this);
+//        header.initWithString("ITIT");
+//        rotateHeaderWebViewFrame.setHeaderView(header);
+//        rotateHeaderWebViewFrame.addPtrUIHandler(header);
+//
+//        rotateHeaderWebViewFrame.disableWhenHorizontalMove(true);
+//
+//        rotateHeaderWebViewFrame.setPtrHandler(new PtrHandler() {
+//            @Override
+//            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+//                boolean canRefresh = PtrDefaultHandler.checkContentCanBePulledDown(frame,
+//                        content, header);
+//                return canRefresh;
+//            }
+//
+//            @Override
+//            public void onRefreshBegin(PtrFrameLayout frame) {
+//                webView.loadUrl(url);
+//                rotateHeaderWebViewFrame.postDelayed(() -> rotateHeaderWebViewFrame
+//                        .refreshComplete(), 10000);//默认10s关闭
+//            }
+//        });
+//    }
 
     private void initWebview() {
         if (webView == null) return;
@@ -230,9 +286,9 @@ public class WrapperActivity extends SwipeBackActivity {
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
             WrapperActivity.this.url = url;
-            if (rotateHeaderWebViewFrame != null) {
-                rotateHeaderWebViewFrame.refreshComplete();
-            }
+//            if (rotateHeaderWebViewFrame != null) {
+//                rotateHeaderWebViewFrame.refreshComplete();
+//            }
         }
 
         @Override
